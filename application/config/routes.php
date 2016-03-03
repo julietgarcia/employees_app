f9e64209d609c9f5a3f2f5ccd8eb24490b5b4c03
Juliet Garcia <juliet.garcia@outlook.com>
2016-03-03 07:59:23 +0100

    In develop progress

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,8 +38,10 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "general/login";
 $route['404_override'] = '';
+
+$route['login'] = "login";
 
 
 /* End of file routes.php */
---
+++ application/views/employees.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('layouts/default-header.php'); ?>
+
+
+
+<?php $this->load->view('layouts/default-footer.php'); ?>
